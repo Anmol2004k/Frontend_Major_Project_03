1b02f6b31837c6099458f612784cd25fddbc38fa
Anmol Kumar <at4141anmol@gmail.com>
2025-09-23 17:09:49 +0530

    adding Swiper Animations on pages

--- script.js
+++ script.js
@@ -4,3 +4,48 @@ const scroll = new LocomotiveScroll({
 });
 
 
+function page4Animation() {
+
+    var elemC = document.querySelector("#elem-con")
+var fixed = document.querySelector("#fixed-img")
+
+elemC.addEventListener("mouseenter", function () {
+    fixed.style.display = "block";
+
+})
+
+elemC.addEventListener("mouseleave", function () {
+    fixed.style.display = "none";
+
+})
+
+var elemdot = document.querySelectorAll(".elem")
+elemdot.forEach(function (e) {
+    e.addEventListener("mouseenter", function () {
+        var imgee = e.getAttribute("data-image")
+        fixed.style.backgroundImage = `url(${imgee})`
+
+    })
+
+})
+    
+}
+
+
+
+function swiperAnimation() {
+
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: "auto",
+        centeredSlides: true,
+        spaceBetween: 30,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+    });
+
+}
+
+swiperAnimation();
+page4Animation();
